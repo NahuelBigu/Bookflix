0da9a5e5d2523d5a54872f8bf52d00843371824a
fenrir450 <64496921+fenrir450@users.noreply.github.com>
2020-05-02 20:02:31 -0300

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,14 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
-    email: String,
-    password: String
+    email: { type: String, required: true},
+    password: { type: String, required: true},
+    plan: { type: String, required: true},
+    name: { type: String, required: true},
+    creditCardNumber: { type: Number, required: true},
+    expiration: {type: Date, required: true},
+    CVV: {type: Number, required: true},
+    historial: Array
 }, {
     timestamps: true
 });
