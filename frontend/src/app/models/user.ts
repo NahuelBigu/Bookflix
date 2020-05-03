2f9ff8ab2856628b5c7553b73b1c9da251647e7c
arieldg1997 <arieldg1997@hotmail.com>
2020-05-03 03:54:17 -0300

    Dapia corrigiendo User

--- frontend/src/app/models/user.ts
+++ frontend/src/app/models/user.ts
@@ -2,15 +2,15 @@ import {Profile} from './profile'
 
 export class User {
     _id: string;
-    name: string;
     email: String;
     password: String;
     plan: string;
     creditCard: {
-            cardNumber:  number;
-            expirationMonth: number;
-            expirationYear: number; 
-            CVV: number;
+            creditCardName: string;
+            creditCardNumber:  number;
+            creditCardMM: number;
+            creditCardYY: number; 
+            creditCardCVV: number;
         };
     profiles: Array<Profile>
 }
