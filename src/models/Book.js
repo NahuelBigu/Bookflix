dd80ec612319639ffe2bbdea5b71b476390dcb96
arieldg1997 <arieldg1997@hotmail.com>
2020-05-02 22:29:39 -0300

    Ariel

--- src/models/Book.js
+++ src/models/Book.js
@@ -1,10 +1,12 @@
 const { Schema, model } = require('mongoose');
 
-const BookSchema = new Schema({
-    name: String,
-    author: String
-}, {
-    timestamps: true
-});
+const bookSchemma = new Schema ({
+    name: {type: String, required: true},
+    author: {type: String, required: true},
+    genre: {type: String, required: true},
+    editorial: {type: String, required: true},
+    image: {type: String, required: true},
+    bookPDF: {type: String, required: true},
+})
 
-module.exports = model('Book', BookSchema);
+module.exports = model('book', bookSchemma);
