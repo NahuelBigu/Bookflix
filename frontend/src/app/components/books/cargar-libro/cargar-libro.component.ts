881c0b64b7a0c692cf583930d8b4b7818f24de8d
arieldg1997 <arieldg1997@hotmail.com>
2020-05-17 17:43:43 -0300

    Ariel

--- frontend/src/app/components/books/cargar-libro/cargar-libro.component.ts
+++ frontend/src/app/components/books/cargar-libro/cargar-libro.component.ts
@@ -7,6 +7,8 @@ import { Autor } from '../../../models/autor';
 import { Router } from "@angular/router";
 import { Genero } from 'src/app/models/genero';
 import { GeneroService } from 'src/app/services/genero/genero.service';
+import { Editorial } from 'src/app/models/editorial';
+import { EditorialService } from 'src/app/services/editorial/editorial.service';
 
 @Component({
   selector: 'app-cargar-libro',
@@ -17,18 +19,20 @@ export class CargarLibroComponent implements OnInit {
 
   book: Book = new Book;
   autor: Autor = new Autor;
+  editorial: Editorial = new Editorial;
   genero: Genero = new Genero;
   autors;
+  editoriales;
   generos;
-  editorial: String = '';
 
-  constructor(private bookService: BookService, private router: Router, private autorService: AutorService,private generoService: GeneroService) {
+  constructor(private bookService: BookService, private router: Router, private autorService: AutorService,private generoService: GeneroService,private editorialService: EditorialService) {
 
    }
 
   ngOnInit(): void {
     this.autorService.getAutors().subscribe(data=> this.autors=data as Array<Autor>);
     this.generoService.getGeneros().subscribe(data=> this.generos=data as Array<Genero>);
+    this.editorialService.getEditoriales().subscribe(data=> this.editoriales=data as Array<Editorial>);
   }
 
   addBook(){
@@ -50,6 +54,13 @@ export class CargarLibroComponent implements OnInit {
       this.generoService.postGenero(this.genero)
       .subscribe();
       this.generoService.getGeneros().subscribe(data=> this.generos=data as Array<Genero>);
+    }
+  }
+  addEditorial(){
+    if (this.editorial.name!=''){
+      this.editorialService.postEditorial(this.editorial)
+      .subscribe();
+      this.editorialService.getEditoriales().subscribe(data=> this.editoriales=data as Array<Editorial>);
     }
   }
   
