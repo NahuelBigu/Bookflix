01401313687b9ef189257d90dc5a646b8ff848d7
fenrir450 <64496921+fenrir450@users.noreply.github.com>
2020-05-16 23:08:31 -0300

    Update book.js

--- src/models/book.js
+++ src/models/book.js
@@ -2,6 +2,7 @@ const { Schema, model } = require('mongoose');
 
 const bookSchemma = new Schema({
     name: { type: String, required: true },
+    synopsis: { type: String, required: true },
     author: { type: String, required: true },
     genre: { type: String, required: true },
     editorial: { type: String, required: true },
