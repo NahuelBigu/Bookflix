dc52831baa0565b80f2955835d6134c9aaab4048
NahuelBigu <nahuel.bigu@gmail.com>
2020-06-07 01:32:32 -0300

    Usuarios Tabla

--- frontend/src/app/components/user/user-list/user-list.component.ts
+++ frontend/src/app/components/user/user-list/user-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
+declare var $:any;
 
 @Component({
   selector: 'app-user-list',
@@ -14,6 +15,36 @@ export class UserListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-  }
+    $(document).ready(function() {
+    $('#table-users').DataTable({
+      "bPaginate": "simple", // "simple" option for 'Previous' and 'Next' buttons only
 
+      "bFilter": true,
+      language: {
+          "decimal": "",
+          "emptyTable": "No hay información",
+          "info": "Mostrando _START_ a _END_ de _TOTAL_",
+          "infoEmpty": "Mostrando 0 a 0 de 0",
+          "infoFiltered": "(Filtrado de _MAX_ total)",
+          "infoPostFix": "",
+          "thousands": ",",
+          "lengthMenu": "Mostrar _MENU_",
+          "loadingRecords": "Cargando...",
+          "processing": "Procesando...",
+          "search": "Buscar:",
+          "zeroRecords": "Sin resultados",
+          "paginate": {
+              "first": "Primero",
+              "last": "Ultimo",
+              "next": "Siguiente",
+              "previous": "Anterior"
+          }
+      },
+
+
+  });
+  $('.dataTables_length').addClass('bs-select');
+});
+  }
+  
 }
